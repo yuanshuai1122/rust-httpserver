8b815336dce2b801a7f5c330657f5d16ff7269f2
yuanshuai <yuanshuai@gyyx.cn>
2022-10-07 22:40:54 +0800

    client向server发送消息并得到响应

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,5 +1,12 @@
 use std::net::TcpStream;
+use std::io::{Read, Write};
+use std::str;
 
 fn main() {
-    let _stream = TcpStream::connect("localhost:3000").unwrap();
+    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+
+    let mut buffer = [0;5];
+    stream.read(&mut buffer).unwrap();
+    println!("Response from server:{:?}",str::from_utf8(&buffer).unwrap() );
 }
