9e9023d565f5ec0ff9cd2293453c8ec6642b0475
yuanshuai <yuanshuai@gyyx.cn>
2022-10-07 23:00:03 +0800

    创建HttpRequest的Method和Version枚举并测试

---
+++ http/src/httprequest.rs
@@ -0,0 +1,49 @@
+#[derive(Debug, PartialEq)]
+pub enum Method {
+    Get,
+    Post,
+    Uninitialized,
+}
+
+impl From<&str> for Method {
+    fn from(s: &str) -> Method {
+        match s {
+            "GET" => Method::Get,
+            "POST" => Method::Post,
+            _ => Method::Uninitialized,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized,
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Version {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_method_into() {
+        let m: Method = "GET".into();
+        assert_eq!(m,Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let v: Version = "HTTP/1.1".into();
+        assert_eq!(v, Version::V1_1);
+    }
+}
---
+++ http/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod httprequest;
+mod httpresponse;
